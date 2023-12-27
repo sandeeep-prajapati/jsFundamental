fbc2410f4251bc961c2582109e7d082d8b57120c
sandeeep-prajapti <6392424180sandeep@gmail.com>
2023-12-27 19:06:35 +0530

    sdgujg

---
+++ index.js
@@ -0,0 +1,24 @@
+const pInfo = document.querySelector("#pInfo")
+const name =document.querySelector("#name")
+const dateOfBirth =document.querySelector("#dateOfBirth")
+const contextNumber =document.querySelector("#contextNumber")
+const Whatsapp =document.querySelector("#Whatsapp")
+
+
+
+
+
+
+
+
+
+
+pInfo.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    var name1 = name.value;
+    var dateOfBirth1 = dateOfBirth.value
+    var contextNumber1 = contextNumber.value
+    var Whatsapp = Whatsapp.value
+    if((name1 = "") &&(dateOfBirth1 = "")&&(contextNumber1 = "")&&(naWhatsappme1 = ""))
+    // console.log(name1);
+})
